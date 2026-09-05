27713f58d97c4909637f2261b1cbcdcfd0ba97ba
IPOLIPOL <184828119+IPOLIPOL@users.noreply.github.com>
2026-09-05 13:14:44 +0200

    extension

--- src/main.ml
+++ src/main.ml
@@ -1,19 +1,37 @@
 open Genealogy_types
 
-let people = [ Persons.rudkina_nina; Persons.kotlykov_pavel; Persons.kotlykov_andrei ]
+let people = 
+  [ Persons.rudkina_nina; 
+    Persons.kotlykov_pavel; 
+    Persons.kotlykov_andrei; 
+    Persons.kotlykov_innokentiy; 
+    Persons.afanasyeva_proskovya; 
+    Persons.rudkin_nikolay; 
+    Persons.korotueva_kseniia ]
 
 let parent_child_relations =
   [ Relations.rudkina_nina_mother_of_kotlykov_andrei;
-    Relations.kotlykov_pavel_father_of_kotlykov_andrei ]
+    Relations.kotlykov_pavel_father_of_kotlykov_andrei;
+    Relations.afanasyeva_proskovya_mother_of_kotlykov_pavel;
+    Relations.kotlykov_innokentiy_father_of_kotlykov_pavel;
+    Relations.korotueva_kseniia_mother_of_rudkina_nina;
+    Relations.rudkin_nikolay_father_of_rudkina_nina ]
 
 let marriage_relations =
   [ Relations.rudkina_nina_wife_of_kotlykov_pavel;
-    Relations.kotlykov_pavel_husband_of_rudkina_nina ]
+    Relations.kotlykov_pavel_husband_of_rudkina_nina;
+    Relations.afanasyeva_proskovya_wife_of_kotlykov_innokentiy;
+    Relations.kotlykov_innokentiy_husband_of_afanasyeva_proskovya;
+    Relations.korotueva_kseniia_wife_of_rudkin_nikolay;
+    Relations.rudkin_nikolay_husband_of_korotueva_kseniia]
 
 let () =
   List.iter
     (fun (person : Genealogy_types.person) ->
-      Printf.printf "%s: %s (%s)\n" person.id person.name person.name_ru)
+      Printf.printf "%s: %s (%s)\n" 
+      person.id 
+      person.name 
+      person.name_ru)
     people;
 
   List.iter
--- src/relations.ml
+++ src/relations.ml
@@ -26,11 +26,59 @@ let kotlykov_pavel_husband_of_rudkina_nina =
     confidence = Certain;
   }
 
+let afanasyeva_proskovya_wife_of_kotlykov_innokentiy =
+  {
+    id = ":marriage-2a";
+    subject = ":afanasyeva_proskovya";
+    role = Wife;
+    spouse = ":kotlykov_innokentiy";
+    marriage_date = Unknown;
+    place = Some "Ulan-Ude, USSR";
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let kotlykov_innokentiy_husband_of_afanasyeva_proskovya =
+  {
+    id = ":marriage-2b";
+    subject = ":kotlykov_innokentiy";
+    role = Husband;
+    spouse = ":afanasyeva_proskovya";
+    marriage_date = Unknown;
+    place = Some "Ulan-Ude, USSR";
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let korotueva_kseniia_wife_of_rudkin_nikolay =
+  {
+    id = ":marriage-3a";
+    subject = ":korotueva_kseniia";
+    role = Wife;
+    spouse = ":rudkin_nikolay";
+    marriage_date = Unknown;
+    place = Some "Ulan-Ude, USSR";
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let rudkin_nikolay_husband_of_korotueva_kseniia =
+  {
+    id = ":marriage-3b";
+    subject = ":rudkin_nikolay";
+    role = Husband;
+    spouse = ":korotueva_kseniia";
+    marriage_date = Unknown;
+    place = Some "Ulan-Ude, USSR";
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
   (*   Parent-child relations   *)
 
 let rudkina_nina_mother_of_kotlykov_andrei =
   {
-    id = ":parentage-1";
+    id = ":parentage-1a";
     parent = ":rudkina_nina";
     child = ":kotlykov_andrei";
     relation_type = Biological;
@@ -40,9 +88,49 @@ let rudkina_nina_mother_of_kotlykov_andrei =
 
 let kotlykov_pavel_father_of_kotlykov_andrei =
   {
-    id = ":parentage-2";
+    id = ":parentage-1b";
     parent = ":kotlykov_pavel";
     child = ":kotlykov_andrei";
+    relation_type = Biological;
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let afanasyeva_proskovya_mother_of_kotlykov_pavel =
+  {
+    id = ":parentage-2a";
+    parent = ":afanasyeva_proskovya";
+    child = ":kotlykov_pavel";
+    relation_type = Biological;
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let kotlykov_innokentiy_father_of_kotlykov_pavel =
+  {
+    id = ":parentage-2b";
+    parent = ":kotlykov_innokentiy";
+    child = ":kotlykov_pavel";
+    relation_type = Biological;
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let korotueva_kseniia_mother_of_rudkina_nina =
+  {
+    id = ":parentage-3a";
+    parent = ":korotueva_kseniia";
+    child = ":rudkina_nina";
+    relation_type = Biological;
+    source = Some ":parish-record-17";
+    confidence = Certain;
+  }
+
+let rudkin_nikolay_father_of_rudkina_nina =
+  {
+    id = ":parentage-3b";
+    parent = ":rudkin_nikolay";
+    child = ":rudkina_nina";
     relation_type = Biological;
     source = Some ":parish-record-17";
     confidence = Certain;
